e2e13ab06a8b5a2df7b7da666926ee85e58e0f44
Nicolas Coulombeau <nicolas.coulombeau@gmail.com>
2019-12-09 13:48:32 +0100

    exercice 1 complexity optimization

--- exercises.py
+++ exercises.py
@@ -26,11 +26,18 @@ def is_anagram(s1, s2):
     if len(s1) != len(s2):
         return False
 
+    hash_map = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0,
+                "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0}
+
     for letter in s1:
-        index_in_s2 = s2.find(letter)
-        if index_in_s2 == -1:
+        hash_map[letter] += 1
+
+    for letter in s2:
+        hash_map[letter] -= 1
+
+    for letter in hash_map.keys():
+        if hash_map[letter] != 0:
             return False
-        s2 = s2.replace(letter, '', 1)
 
     return True
 
